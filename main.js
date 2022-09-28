b02d45ffbaa49ccbff17b7915ed49533c3efe4a3
John Doe <jlfloressanchez01@gmail.com>
2022-09-27 21:41:29 -0400

    Third

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 function myFunction1() {
+    let user = prompt("Insert your first name")
+    if (user === "jose") {
+        alert(user + ", Your name is pretty")
+        } else if (user === "javier") {
+            alert(user + ", Youwin a golden ticket")
+        } else {
+        alert(user + ", Your name is not pretty")
+        } 
     
-    document.getElementById("demo").innerHTML = alert("Esto es una prueba")
-}
+    } 
+
 
 
